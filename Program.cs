32fe1e53fbbc12c5f9180c8852b7a4e79f361ae4
khianvictorycalderon <ckhianvictory@gmail.com>
2026-06-09 17:17:13 +0800

    Segmented code in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
+// Builder
 var builder = WebApplication.CreateBuilder(args);
 
+// Services
+
+// Application
 var app = builder.Build();
 
+// Middlewares
+
+// Running the main application
 app.Run();
