d668e40cf3dafb1b33eafb0ced520ab595aa1583
khianvictorycalderon <ckhianvictory@gmail.com>
2026-06-09 21:33:03 +0800

    Added DTOs

--- DTOs/UserDto.cs
+++ DTOs/UserDto.cs
@@ -1,1 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace asp_net_rest_apis_crud.DTOs;
+
+// This is what is send for creating user, and this is also what is returned when getting a user
+public class UserDto
+{
+    [Required]
+    public string FirstName { get; set; } = null!;
+    
+    public string? MiddleName { get; set; }
+
+    [Required]
+    public string LastName { get; set; } = null!;
+
+    [Required]
+    public DateTime BirthDate { get; set; }
+}
+
+public class GetUserResponseDto
+{
+    public string FirstName { get; set; } = null!;
+    public string MiddleName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public DateTime BirthDate { get; set; }
+}
+
+public class GetAllUsersResponseDto
+{
+    public string message { get; set; } = null!;
+    public List<GetUserResponseDto> Users { get; set; } = [];
+
+}
