e38dc2ec6a0ba650a25d70f9ad8495e5ae142945
khianvictorycalderon <ckhianvictory@gmail.com>
2026-06-09 17:18:41 +0800

    Added controllers

--- Program.cs
+++ Program.cs
@@ -5,12 +5,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Services
 builder.Services.AddApiCors(builder.Configuration);
+builder.Services.AddControllers();
 
 // Application
 var app = builder.Build();
 
 // Middlewares
 app.UseApiCors();
+app.MapControllers();
 
 // Running the main application
 app.Run();
