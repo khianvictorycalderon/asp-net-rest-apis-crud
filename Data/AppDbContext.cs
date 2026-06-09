e6cc3cbdfe354ab194bccf5598520a96a71756fc
khianvictorycalderon <ckhianvictory@gmail.com>
2026-06-09 17:23:21 +0800

    Added DB Context

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,1 +1,8 @@
 namespace asp_net_rest_apis_crud.Data;
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext (DbContextOptions<AppDbContext> options)
+    : base(options) {}
+}
