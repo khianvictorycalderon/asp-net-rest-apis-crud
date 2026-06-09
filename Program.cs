5f51e85cd77ccdfbb701108bcbec9d63cec65a75
khianvictorycalderon <ckhianvictory@gmail.com>
2026-06-09 20:53:11 +0800

    Registered database context

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
+using asp_net_rest_apis_crud.Data;
 using asp_net_rest_apis_crud.Extensions;
+using Microsoft.EntityFrameworkCore;
 
 // Builder
 var builder = WebApplication.CreateBuilder(args);
 
 // Services
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddApiCors(builder.Configuration);
 builder.Services.AddControllers();
 
