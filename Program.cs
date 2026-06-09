bb12ba9c0233a619c868b2ba27929377abf019b5
khianvictorycalderon <ckhianvictory@gmail.com>
2026-06-09 17:17:44 +0800

    Registerd API Cors as extension and middleware

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
+using asp_net_rest_apis_crud.Extensions;
+
 // Builder
 var builder = WebApplication.CreateBuilder(args);
 
 // Services
+builder.Services.AddApiCors(builder.Configuration);
 
 // Application
 var app = builder.Build();
 
 // Middlewares
+app.UseApiCors();
 
 // Running the main application
 app.Run();
