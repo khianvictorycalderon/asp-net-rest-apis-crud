1e96b8ab318b5cf3d5b9e6bc28466c86102eacde
khianvictorycalderon <ckhianvictory@gmail.com>
2026-06-09 17:15:08 +0800

    Added AddApiCors service

---
+++ Extensions/CorsExtension.cs
@@ -0,0 +1,29 @@
+namespace asp_net_rest_apis_crud.Extensions;
+
+public static class CorsExtension
+{
+    public const string PolicyName = "ApiCors";
+
+    public static IServiceCollection AddApiCors(
+        this IServiceCollection services,
+        IConfiguration config
+    )
+    {
+        var origins = config
+            .GetSection("Cors:AllowedOrigins")
+            .Get<string[]>() ?? Array.Empty<string>(); // For backwards compatibility
+
+        services.AddCors(options =>
+        {
+           options.AddPolicy(PolicyName, policy =>
+           {
+              policy
+                .WithOrigins(origins)
+                .WithMethods("POST", "GET", "PUT", "DELETE")
+                .AllowAnyHeader();
+           });
+        });
+
+        return services;
+    }
+}
