088e4e98d0cacf666aa64303a527cb60e86424a4
khianvictorycalderon <ckhianvictory@gmail.com>
2026-06-09 21:38:01 +0800

    Completed user services interfaces

--- Services/IUserServices.cs
+++ Services/IUserServices.cs
@@ -4,5 +4,9 @@ namespace asp_net_rest_apis_crud.Services;
 
 public interface IUserService
 {
-    
+    Task<MessageResponseDto> CreateUser(UserDto dto);
+    Task<GetUserResponseDto> GetUser(Guid id, GetUserResponseDto dto);
+    Task<GetUserResponseDto> GetAllUsers(GetAllUsersResponseDto dto);
+    Task<MessageResponseDto> UpdateUser(Guid id, UserDto dto);
+    Task<MessageResponseDto> DeleteUser(Guid id);
 }
