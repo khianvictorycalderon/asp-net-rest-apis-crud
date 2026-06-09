e37f475a3c5d3ca32d272e51d9d78f57efb1ccd0
khianvictorycalderon <ckhianvictory@gmail.com>
2026-06-09 21:04:18 +0800

    Added User.cs model

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class User
+{
+    public Guid id { get; set; }
+    
+    public string FirstName { get; set; } = null!;
+
+    public string? MiddleName { get; set; }
+
+    public string LastName { get; set; } = null!;
+
+    public DateTime BirthDate { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+}
